6b640ea6384bcb433596c04767e4b11bafd2f95f
Harizky Arfianto <Harizkydps@gmail.com>
2026-07-16 21:13:49 +0700

    add new sample gejson layer

--- webapp-maplibre/src/map.js
+++ webapp-maplibre/src/map.js
@@ -4,4 +4,43 @@ import { Map } from 'maplibre-gl';
 const map = new Map({
     container: 'map', 
     style: 'https://demotiles.maplibre.org/globe.json',
+    center: [110.20971606819703, -7.493319607970615],
+    zoom: 7
+});
+
+const data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Rumah Ayang"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.2096702,
+          -7.4931782
+        ]
+      }
+    }
+  ]
+}
+map.on('load', () => {
+map.addSource('kota',{
+    type: 'geojson',
+    data: data
+});
+
+map.addLayer({
+    id: 'kota-layer',
+    type: 'circle',
+    source: 'kota',
+    paint: {
+        'circle-radius': 7,
+        'circle-color': '#ff0000',
+        'circle-stroke-width': 1,
+        'circle-stroke-color': '#ffffff'
+    }
+})
 });
